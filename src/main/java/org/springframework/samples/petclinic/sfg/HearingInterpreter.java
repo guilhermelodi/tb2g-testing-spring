f95e186de322885f628c70129e0c3ee5bb3351f9
Guilherme Lodi <guilherme.tlodi@gmail.com>
2020-09-12 23:46:57 -0300

    JUnit 4 - Implementation of Yanny and Laurel tests

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/HearingInterpreter.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HearingInterpreter {
+
+    private final WordProducer wordProducer;
+
+    public HearingInterpreter(WordProducer wordProducer) {
+        this.wordProducer = wordProducer;
+    }
+
+    public String whatIHeard() {
+        final String word = wordProducer.getWord();
+
+        System.out.println(word);
+
+        return word;
+    }
+
+}
