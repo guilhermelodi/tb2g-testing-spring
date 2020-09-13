efac2602ecd1c42a618e4b7b830fe3fa2c7dc3f7
Guilherme Lodi <guilherme.tlodi@gmail.com>
2020-09-13 00:11:07 -0300

    JUnit 5 - Implementation of Yanny and Laurel tests

--- src/test/java/org/springframework/samples/petclinic/sfg/junit4/YannyHearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit4/YannyHearingInterpreterTest.java
@@ -1,8 +1,11 @@
-package org.springframework.samples.petclinic.sfg;
+package org.springframework.samples.petclinic.sfg.junit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.YannyConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
